b3acf241e957487a20d0f0a1a1392659ae5a7172
Matyacc <89623391+Matyacc@users.noreply.github.com>
2022-07-28 11:04:31 -0300

    Primer carga

---
+++ repositorio.bat
@@ -0,0 +1,7 @@
+echo "# Gsolutions" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MMSPACK/Gsolutions.git
+git push -u origin main
